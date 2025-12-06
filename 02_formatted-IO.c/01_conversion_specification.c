f0a147c9921ae2dfc4bd2bde91ef2c3be615a97a
sushil sunar <sunarsuhil100@gmail.com>
2025-12-06 19:36:52 +0545

    done some exercise && formatted IO has started

---
+++ 02_formatted-IO.c/01_conversion_specification.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+  int i ,j;
+  float x,y;
+
+  i = 10;
+  j = 20;
+
+  x = 43.9348f;
+  y = 89.534f;
+
+  // %d and %f are conversion specifications.
+  // which means some binary value  will come in
+  // and convert that in to character accordingly.
+  // e.g %d is the placeholder for int
+  // %f for the float and so on.
+  
+  printf("i = %d | j = %d | x = %f | y = %f",i,j,x,y);
+  return 0;
+}
