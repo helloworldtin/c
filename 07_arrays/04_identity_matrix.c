140dfbbaaa9fa2155fa55f0111296226595592a6
sushil sunar <sunarsuhil100@gmail.com>
2026-02-11 21:25:41 +0545

    learn array

---
+++ 07_arrays/04_identity_matrix.c
@@ -0,0 +1,18 @@
+#define N 10
+
+int main(void){
+    double ident[N][N];
+
+    int row,col;
+
+    for(row = 0; row < N; row++){
+        for(col = 0; col < N; col++){
+            if(row == col) {
+                ident[row][col] = 1;
+            }else {
+                ident[row][col] = 0;
+            }
+        }
+    }
+    return 0;
+}
